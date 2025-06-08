6e9bf856d594141d683c250b6c2117a854077095
Najmunda <najmundaakmal@gmail.com>
2025-06-08 13:14:18 +0700

    feat: Init repo for deploy

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+export default function(eleventyConfig) {
+  eleventyConfig.setServerOptions({
+    showAllHosts: true,
+  });
+  eleventyConfig.addPassthroughCopy("src/assets/css/style.css");
+  eleventyConfig.addPassthroughCopy("src/assets/fonts/PlusJakartaSans-VariableFont_wght.ttf");
+  eleventyConfig.addPassthroughCopy("src/assets/images");
+  eleventyConfig.addPassthroughCopy("src/assets/site.webmanifest");
+  return {
+    dir: {
+      "input": "src",
+      "data": "_data",
+      "includes": "_includes",
+      "layouts": "_layouts",
+    }
+  }
+};
