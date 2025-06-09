7560b94fd82d88a9ff9c73385714d3a4f3575e7f
Najmunda <najmundaakmal@gmail.com>
2025-06-09 11:28:11 +0700

    fix: build-ghpages script & undo addPassThrough config

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,10 @@ export default function(eleventyConfig) {
   eleventyConfig.setServerOptions({
     showAllHosts: true,
   });
-  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/assets/css/style.css");
+  eleventyConfig.addPassthroughCopy("src/assets/fonts/PlusJakartaSans-VariableFont_wght.ttf");
+  eleventyConfig.addPassthroughCopy("src/assets/images");
+  eleventyConfig.addPassthroughCopy("src/assets/site.webmanifest");
   return {
     pathPrefix: "/",
     dir: {
